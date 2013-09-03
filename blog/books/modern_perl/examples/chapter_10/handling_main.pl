57bece286f6a39aab8ce0002f97b5117e3fea6c0
fei <philips.ni.fei@gmail.com>
2013-09-02 17:54:03 -0700

    finishe chapter_10 Controlled Execution

---
+++ blog/books/modern_perl/examples/chapter_10/handling_main.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use Data::Dumper;
+
+exit main(@ARGV);
+
+
+
+sub main{
+    my @ARGV = @_;
+    print Dumper \@ARGV;
+    my $i = 0;
+    print "i: $i\n";
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
