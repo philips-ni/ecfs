b97d52b385b7ac2559219d6173d733641c96df2d
philips.ni.fei@gmail.com <philips.ni.fei@gmail.com>
2018-04-25 18:37:09 -0700

    add customer page

--- shine_project/shine/Gemfile
+++ shine_project/shine/Gemfile
@@ -22,6 +22,7 @@ gem 'coffee-rails', '~> 4.2'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 gem 'devise'
+gem 'faker'
 gem 'foreman'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- shine_project/shine/db/seeds.rb
+++ shine_project/shine/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10_000.times do |i|
+  Customer.create!(
+		   first_name: Faker::Name.first_name,
+		   last_name: Faker::Name.last_name,
+		   username: "#{Faker::Internet.user_name}#{i}",
+		   email: Faker::Internet.user_name + i.to_s + "@veritas.com"
+		   )
+  print '.' if i % 1000 == 0
+end
+puts
+
